5d048772659883bd6ca635df6397db44d0eca5c5
Manohar Mukku <manoharmukku@gmail.com>
2018-04-07 14:05:12 +0530

    Solved problem Weight of Numbers

--- contests/2018-april-long/WGHTNUM.c
+++ contests/2018-april-long/WGHTNUM.c
@@ -3,4 +3,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define m 1e9 + 7
+#define M 1000000007
+
+unsigned long findPower(int x, unsigned long long N) {
+	if (N == 0) return 1;
+	if (N == 1) return x;
+
+	unsigned long aux = findPower(x, N/2);
+	unsigned long long result = (aux * aux) % M;
+
+	// If N is odd
+	if (N & 1ULL) {
+		result = (x * result) % M;
+	}
+
+	return (unsigned long) result;
+}
+
+unsigned long findDigitsWithWeight(int W, unsigned long long N) {
+	if (W < -9 || W > 8) return 0;
+
+	if (W >= 0)
+		return ((9 - W) * findPower(10, N-2)) % M;
+	else
+		return ((10 + W) * findPower(10, N-2)) % M;
+}
+
+int main() {
+	int T;
+	scanf ("%d", &T);
+
+	while (T--) {
+		unsigned long long N;
+		scanf ("%llu", &N);
+
+		int W;
+		scanf ("%d", &W);
+
+		printf("%lu\n", findDigitsWithWeight(W, N));
+	}
+
+	return 0;
+}
