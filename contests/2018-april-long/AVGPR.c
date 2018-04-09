b3a6342da8f9b0c1bf51e598452bc0c637fc11b5
Manohar Mukku <manoharmukku@gmail.com>
2018-04-09 18:15:34 +0530

    Deleted AVGPR.cpp

--- contests/2018-april-long/AVGPR.c
+++ contests/2018-april-long/AVGPR.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #define TRUE 1
+#define RANGE 1000
 
 int main() {
 	int T;
@@ -16,25 +17,22 @@ int main() {
 		int i, j;
 		int* A = (int*)malloc(N * sizeof(int));
 
-		char* isPresent = (char*)calloc(1001, sizeof(char));
+		int* presentCount = (int*)calloc(2*RANGE+1, sizeof(int));
 
 		for (i = 0; i < N; i++) {
 			scanf ("%d", &A[i]);
-			isPresent[A[i]] = TRUE;
+			presentCount[A[i] + RANGE]++;
 		}
 
-		int count = 0;
-		for (i = 0; i < N; i++) {
-			for (j = i+1; j < N; j++) {
-				if ((A[i] + A[j]) % 2 == 0 && isPresent[(A[i] + A[j]) / 2])
-					count++;
-			}
+		int i, count = 0;
+		for (i = -RANGE; i <= RANGE; i++) {
+			
 		}
 
 		printf("%d\n", count);
 
 		free(A);
-		free(isPresent);
+		free(presentCount);
 	}
 
 	return 0;
